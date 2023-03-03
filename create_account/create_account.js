8f9d45eda1aa0f5c49521da282252cca57646fe1
barnabas <xobarneysolomon@gmail.com>
2023-03-03 23:42:57 +0300

    finished create 2 and stopped submitting on enter

--- create_account/create_account.js
+++ create_account/create_account.js
@@ -11,6 +11,7 @@ let fname_field = document.querySelector(".fname");
 let lname_field = document.querySelector(".lname");
 let email_field = document.querySelector(".email");
 let phone_field = document.querySelector(".phone");
+let acc_type_field = document.querySelector("#account_type_input"); 
 
 let create_1 = document.querySelector(".create_1");
 let create_2 = document.querySelector(".create_2");
@@ -22,6 +23,13 @@ let bar_3 = document.querySelector(".bar3");
 
 let next_btn = document.querySelector(".next_btn");
 let prev_btn = document.querySelector(".prev_btn");
+
+//PREVENTING SUBMISSION WHEN ENTER KEY IS PRESSED
+document.addEventListener("keydown", function(event){
+  if (event.key === "Enter"){
+    event.preventDefault();
+  }
+});
 
 //VALIDATING FUNCTIONS
 function is_email(str) {
@@ -36,10 +44,18 @@ function is_letters(str) {
   const regex = /^[a-zA-Z]+$/;
   return regex.test(str);
 }
+function is_account_type(){
+  if (acc_type_field.value == "Account Type"){
+    return false;
+  }
+  return true
+}
 
 //THE NEXT BUTTON
 next_btn.addEventListener("click", function () {
-  let all_true = true;
+  let all_true_1 = true;
+  let all_true_2 = true;
+  let all_true_3 = true;
   // fname_field.style.boxShadow = "none";
   // err_fname.style.display = "none";
   // lname_field.style.boxShadow = "none";
@@ -49,28 +65,32 @@ next_btn.addEventListener("click", function () {
   // phone_field.style.boxShadow = "none";
   // err_phone.style.display = "none";
   // if (is_letters(fname_field.value) == false) {
-  //   all_true = false;
+  //   all_true_1 = false;
   //   fname_field.style.boxShadow = "0px 0px 20px red";
   //   err_fname.style.display = "flex";
   // }
   // if (!is_letters(lname_field.value)) {
-  //   all_true = false;
+  //   all_true_1 = false;
   //   lname_field.style.boxShadow = "0px 0px 20px red";
   //   err_lname.style.display = "flex";
   // }
   // if (is_email(email_field.value) == false) {
   //   console.log('email function')
-  //   all_true = false;
+  //   all_true_1 = false;
   //   email_field.style.boxShadow = "0px 0px 10px red";
   //   err_email.style.display = "flex";
   // }
   // if (!is_number(phone_field.value)) {
-  //   all_true = false;
+  //   all_true_1 = false;
   //   phone_field.style.boxShadow = "0px 0px 20px red";
   //   err_phone.style.display = "flex";
   // }
+  if(is_account_type() == false){
+    all_true_1 = false;
+    acc_type_field
+  }
 
-  if (all_true) {
+  if (all_true_1) {
     bar_1.style.backgroundColor = "white";
     bar_2.style.backgroundColor = "purple";
 
--- create_account/create_account.php
+++ create_account/create_account.php
@@ -1,4 +1,12 @@
 <?php
+echo $_POST['fname'];
+echo "<br>";
+echo $_POST['lname'];
+echo "<br>";
+echo $_POST['email'];
+echo "<br>";
+echo $_POST['phone'];
 
 
 ?>
+
